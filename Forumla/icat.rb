3b2dbf5daacb5318c22d399a936f385d23d3edc9
David Cloutier <dcloutier@strozfriedberg.com>
2018-06-14 09:59:00 -0400

    icat.rb cleanup

--- Forumla/icat.rb
+++ Forumla/icat.rb
@@ -1,24 +1,12 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Icat < Formula
   desc "Outputs an image on a 256-color or 24-bit color enabled terminal with UTF-8 locale, such as gnome-terminal, konsole or rxvt-unicode (urxvt)."
   homepage "https://github.com/atextor/icat"
   url "https://github.com/quaboag/homebrew-brew/blob/master/Archives/icat-0.5.tar.gz"
   version "0.5"
   sha256 "1d77f20c7eab29efe22aeebe426301b7dca1f898759c63f32a714c7c9ae1aab4"
-  # depends_on "cmake" => :build
   depends_on "imlib2" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # system "./configure", "--disable-debug",
-    #                       "--disable-dependency-tracking",
-    #                       "--disable-silent-rules",
-    #                       "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    # system "make", "install" # if this fails, try separate make/make install steps
     system "make"
     bin.install "icat"
   end
